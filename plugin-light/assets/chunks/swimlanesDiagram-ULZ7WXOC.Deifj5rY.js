3f1c9357a0ae803d25cd5c17de38c2fb620012e1
novlan1 <1576271227@qq.com>
2026-08-13 11:47:06 +0000

    docs: update docs of plugin-light. 2026-08-13 11:46:47

---
+++ plugin-light/assets/chunks/swimlanesDiagram-ULZ7WXOC.Deifj5rY.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DBW9Uz4q.js";import{_ as a}from"../app.DB1zTF7W.js";import"./chunk-5VM5RSS4.Dcg_jcTK.js";import"./chunk-XXDRQBXY.Wy0M7I_l.js";import"./chunk-KBJHAD2P.BblhiO0_.js";import"./chunk-2GRJ4B5K.DmkfaOjk.js";import"./channel.D_oDseSf.js";import"./framework.C4Wqxybl.js";import"./theme.Ct74bKjT.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
