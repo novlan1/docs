ef06c034ffc5c5ab5a658f91d9079c2401f808b5
novlan1 <1576271227@qq.com>
2026-08-27 09:22:09 +0000

    docs: update docs of plugin-light. 2026-08-27 09:21:52

--- plugin-light/assets/chunks/swimlanesDiagram-VR7AAH4N.BRbDcWXQ.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW.XfSE2w2N.js";import{_ as a}from"../app.B9cZPCZU.js";import"./chunk-5VM5RSS4.D3vPhw0i.js";import"./chunk-XXDRQBXY.BSJ9ANsX.js";import"./chunk-POPQ4Y6H.BTCkB8eL.js";import"./chunk-F27PBJKO.DziNmSUG.js";import"./channel.DRdJlR4D.js";import"./framework.C4Wqxybl.js";import"./theme.Dbina0wt.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
---
+++ plugin-light/assets/chunks/swimlanesDiagram-VR7AAH4N.Dj-3U9Zm.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.CNt7rrBu.js";import{_ as a}from"../app.CEcDa-jy.js";import"./chunk-5VM5RSS4.DEoJ72kc.js";import"./chunk-XXDRQBXY.BM9xJ1-I.js";import"./chunk-POPQ4Y6H.CTQXayHX.js";import"./chunk-F27PBJKO.xD7EE905.js";import"./channel.pP7-7Hce.js";import"./framework.C4Wqxybl.js";import"./theme.CTcUjU68.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
